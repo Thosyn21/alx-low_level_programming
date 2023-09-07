ca22baedd7a8b81868e5af5b8c31d0e925f16bed
Thosyn21 <thosyn21@gmail.com>
2023-09-07 07:16:27 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Reurn: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char);
+	printf("Size of an int: %d bytes(s)\n", sizeof(int);
+	prontf("Size of a long int: %d bytes(s)\n", sizeof(long int);
+	printf("Size of a lon long int: %d bytes(s)\n", sizeof(long long int);
+	printf("Size of a float: %d bytes(s)\n", sizeof(float);
+
+	return (0);
+}
