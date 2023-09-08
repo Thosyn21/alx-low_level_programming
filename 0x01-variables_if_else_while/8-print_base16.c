6a51539cb648314a16f1d74acc0d2e20872b4d52
Thosyn21 <thosyn21@gmail.com>
2023-09-08 12:46:01 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Task 8
+ * Description - base 26 lower case
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int num;
+	char afabets;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+	putchar(num)
+	}
+	for (afabets = 'a'; afabets <= 'f'; afabets++)
+	{
+	putchar(afabets)
+	}
+	putchar('\n')
+
+	return (0);
+}
