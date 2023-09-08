3c6242304aaa48359c971c0c9fc4c544ebaf396f
Thosyn21 <thosyn21@gmail.com>
2023-09-08 13:17:47 -0700

    9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,15 +11,13 @@ int main(void)
 	int num;
 	char afabets;
 
-	for (num = '0'; num <= '9'; num++)
-	{
 	putchar(num);
 	}
 	for (afabets = 'a'; afabets <= 'f'; afabets++)
 	{
 	putchar(afabets);
 	}
-	putchar('\n');
+	ptchar('\n');
 
 	return (0);
 }
