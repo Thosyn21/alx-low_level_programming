aae5336e69ec630a97a3eae4d0063852b3f4a66e
Thosyn21 <thosyn21@gmail.com>
2023-09-08 11:39:41 -0700

    3

---
+++ 0x01-variables_if_else_while/triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+       	char i;
+	char j;
+
+	for (i = 1; i <= 5; i++)
+	{	{
+			for (j = i; j <= i; j++)
+			putchar('*')
+		}
+	putchar('\n')
+	}
+	return (0);
+}
