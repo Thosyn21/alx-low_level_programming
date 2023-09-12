4ee0ee4357238c23083f362a9fe5990b1d3263d7
Thosyn21 <thosyn21@gmail.com>
2023-09-12 22:50:39 +0100

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Task 10
+ * @i: An integer i
+ * @j: An integer j
+ * Description - adds two integers and returns the result
+ * Return: Result
+ */
+int add(int i, int j)
+{
+	return (a + b);
+}
