41dad9de908f023bf1db96fc9a54db7dd38d8fa2
Thosyn21 <thosyn21@gmail.com>
2023-09-12 18:43:58 +0100

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
