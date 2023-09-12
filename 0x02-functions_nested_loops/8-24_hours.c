0af7be90222e05585a95dcf783c28ae6d3cc5bd5
Thosyn21 <thosyn21@gmail.com>
2023-09-12 22:09:02 +0100

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Task 8
+ * Description - prints every minute of the day
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++);
+	{
+		for (M = 0; M < 60; M++);
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putcahr((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
