88c257923024fc749a4326e2c7daa21394b8edc3
Thosyn21 <thosyn21@gmail.com>
2023-09-12 21:44:22 +0100

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Task 6
+ * Description - computes the absolute value of an integer
+ * @r - 
+ * Return: Absolute value of r
+ */
+
+int _abs(int r)
+
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
