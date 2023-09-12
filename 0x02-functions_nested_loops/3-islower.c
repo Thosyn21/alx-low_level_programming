67d636f15654e6638743ef411fd4fa0ea8bc1493
Thosyn21 <thosyn21@gmail.com>
2023-09-12 19:43:42 +0100

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Task 3
+ * Description - checks for lowercase letters
+ * @c: an input character
+ * Return 1 if c is lowercase
+ * Return 0 otherwise
+ */
+
+int _islower(int c);
+{
+		char i;
+			int lower = 0;
+
+				for (i = 'a'; i <= 'z'; i++)
+						{
+									if (i == c)
+													lower = 1;
+										}
+
+					return (lower);
+}
