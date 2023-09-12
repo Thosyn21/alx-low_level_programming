10c1136c9ea72dbf16bae5371b76b65c855b8b1b
Thosyn21 <thosyn21@gmail.com>
2023-09-12 23:07:02 +0100

    11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Task 11
+ * @n: An input integer 
+ * Description - prints all natural numbers
+ * Result: 0
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			prinf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf(", ");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
