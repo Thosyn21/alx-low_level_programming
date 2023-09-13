b932ed5105fe49486282d647321791a37978db5c
Thosyn21 <thosyn21@gmail.com>
2023-09-13 01:21:56 +0100

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,19 +2,17 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * main - writes the character c to stdout
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 
 int main(void)
 {
-	char word[4] = "Main";
+	char c;
 	int i;
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(word[i]);
 	_putchar('\n');
 
